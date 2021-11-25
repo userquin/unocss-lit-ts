01dc471247fccd01f6ae0208771765452eea470a
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-11-25 22:33:43 +0100

    chore: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # unocss-lit-ts
+
+This repo will show you how to use [unocss](https://github.com/antfu/unocss) with [lit]().
+
+The `main` branch contains the code to run a simple `lit web-component`.
+
+We are exploring `scoping css`, you can see a first attempt to make it working with `unocss per-module` on `permodule` branch. 
+
+This repo is using `pnpm`, so if you don't have installed run `npm i -g pnpm` or just use the `package manager` you want.
+
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import UnoCss from 'unocss/vite'
+import { presetUno, presetAttributify } from 'unocss'
+import ViteInspector from 'vite-plugin-inspect'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/my-element.ts',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: /^lit/
+    }
+  },
+  plugins: [
+    UnoCss({
+      include: [/\.ts$/],
+      presets: [
+          presetAttributify(),
+          presetUno()
+      ],
+      inspector: false,
+    }),
+    ViteInspector()
+  ]
+})
